99fd0cd42f5235227ed6ae93f109094762f68422
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2026-07-02 07:41:32 +0700

    Update : Config

--- config.py
+++ config.py
@@ -4,7 +4,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Telegram
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
+# WhatsApp
 WA_TARGET_NUMBER = os.getenv("WA_TARGET_NUMBER")
+WA_API_URL = os.getenv(
+    "WA_API_URL",
+    "http://localhost:3000"
+)
+
+# Database
+DATABASE_PATH = os.getenv(
+    "DATABASE_PATH",
+    "database/botwatel.db"
+)
+
+# Logging
+LOG_LEVEL = os.getenv(
+    "LOG_LEVEL",
+    "INFO"
+)
